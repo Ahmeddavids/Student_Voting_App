d255f424ee4ef37e946295b03eb831b5d1432b48
ahmed isiaka <ahmeddavids6@gmail.com>
2024-07-25 11:02:23 +0100

    removed the get one as it is already existing in the candidate controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -429,22 +429,3 @@ exports.signOut = async (req, res) => {
     }
 };
 
-exports.getOne = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await userModel.findById(id);
-        if(!user){
-            return res.status(404).json({
-                message: 'User not found'
-            })
-        }
-        res.status(200).json({
-            message: 'User details found',
-            data: user
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-}
